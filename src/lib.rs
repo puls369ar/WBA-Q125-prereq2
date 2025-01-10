86e48b049af27df702eea4401807a2d3b1e66bc7
artajohan369 <artajohan369@gmail.com>
2025-01-10 10:23:16 +0400

    fixed github nickcname

--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,80 @@
+mod programs;
 
 #[cfg(test)]
 mod tests {
-	use bs58;
-	use std::io::{self, BufRead};
-	use solana_sdk;
-	//use solana_sdk::{signature::{Keypair, Signer}, pubkey::Pubkey};
-	use solana_client::rpc_client::RpcClient; use solana_sdk::{
 
-		signature::{Keypair, Signer, read_keypair_file} 
-	};
+    use crate::programs::Turbin3_prereq::{WbaPrereqProgram, CompleteArgs, UpdateArgs};
+    use solana_client::rpc_client::RpcClient;
+    use solana_program::{
+        pubkey::Pubkey,
+        system_instruction::transfer,
+        system_program,
+    };
 
-	const RPC_URL: &str = "https://api.devnet.solana.com";
+    use solana_sdk::{
+        signature::{Keypair, Signer, read_keypair_file},
+        transaction::Transaction,
+    };
 
-	#[test]	
-	fn airdrop() {
-		let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
-		let client = RpcClient::new(RPC_URL);
+    const RPC_URL: &str = "https://api.devnet.solana.com";
 
-		match client.request_airdrop(&keypair.pubkey(), 2_000_000_000u64) {
+    #[test]
+    fn enroll() {
+        let rpc_client = RpcClient::new(RPC_URL);
 
-			Ok(s) => {
-			println!("Success! Check out your TX here:");
-			println!("https://explorer.solana.com/tx/{}?cluster=devnet", s.to_string());
+        let signer = read_keypair_file("Turbin3-wallet.json").expect("Couldn't find wallet file");
 
-		},
+        let prereq = WbaPrereqProgram::derive_program_address(&[b"prereq", signer.pubkey().to_bytes().as_ref()]);
 
-		Err(e) => println!("Oops, something went wrong: {}", e.to_string()) };
-	}
+        let args = CompleteArgs { github: b"puls369ar".to_vec() };
 
+        let blockhash = rpc_client
+            .get_latest_blockhash()
+            .expect("Failed to get recent blockhash");
 
-//	#[test]
-//	fn keygen() {
-//
-//		let kp = Keypair::new();
-//		println!("You've generated a new Solana wallet: {}", kp.pubkey().to_string()); println!("");
-//		println!("To save your wallet, copy and paste the following into a JSON file:");
-//
-//		println!("{:?}", kp.to_bytes());
-//
-//	}
-//
-//	#[test]
-//	fn base58_to_wallet() {
-//		println!("Input your private key as base58:");
-//		let stdin = io::stdin();
-//		let base58 = stdin.lock().lines().next().unwrap().unwrap(); println!("Your wallet file is:");
-//		let wallet = bs58::decode(base58).into_vec().unwrap(); println!("{:?}", wallet);
-//	}
-//
-//	#[test]	
-//	fn wallet_to_base58() {
-//
-//		println!("Input your private key as a wallet file byte array:"); 
-//		let stdin = io::stdin(); let wallet = stdin.lock().lines().next().unwrap().unwrap().trim_start_matches('[').trim_end_matches(']').split(',') .map(|s| s.trim()
-//			.parse::<u8>().unwrap())
-//			.collect::<Vec<u8>>();
-//
-//		println!("Your private key is:");
-//		let base58 = bs58::encode(wallet).into_string(); println!("{:?}", base58);
-//
-//	}	
+        if !rpc_client.get_account(&prereq).is_ok() {
+            println!("Account does not exist. Proceeding with allocation and initialization.");
+
+            let transaction = WbaPrereqProgram::complete(
+                &[&signer.pubkey(), &prereq, &system_program::id()],
+                &args,
+                Some(&signer.pubkey()),
+                &[&signer],
+                blockhash,
+            );
+
+            let signature = rpc_client
+                .send_and_confirm_transaction(&transaction)
+                .expect("Failed to send transaction");
+
+            println!(
+                "Success! Check out your TX here:https://explorer.solana.com/tx/{}/?cluster=devnet",
+                signature
+            );
+        } else {
+            println!("Account already exists. Skipping allocation and initialization.");
+
+            // Handle the case where the PDA already exists
+            // You can perform updates, re-use the existing account, or return an error.
+            let update_args = UpdateArgs { github: b"puls369ar".to_vec() };
+
+            let transaction = WbaPrereqProgram::update(
+                &[&signer.pubkey(), &prereq, &system_program::id()],
+                &update_args,
+                Some(&signer.pubkey()),
+                &[&signer],
+                blockhash,
+            );
+
+            let signature = rpc_client
+                .send_and_confirm_transaction(&transaction)
+                .expect("Failed to send update transaction");
+
+            println!(
+                "Success! Check out your Update TX here:https://explorer.solana.com/tx/{}/?cluster=devnet",
+                signature
+            );
+        }
+    }
 }
+
